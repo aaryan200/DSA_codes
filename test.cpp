7a1190789adc6210c0f32a3d9860d746482d29fd
aaryan200 <co21btech11001@iith.ac.in>
2023-01-17 18:22:26 +0530

    commit

--- test.cpp
+++ test.cpp
@@ -20,48 +20,15 @@ inline ll msb(ll n) {return (1 << (31 - __builtin_clz(n)));}
 // Fill whole array with 0.
 // memset(arr, 0, n*sizeof(arr[0]));
 
-void solve() {
-    int n;
-    cin>>n;
-    vector<int> a(n,0);
-    vector<int> check(101,0); bool ch=false;
-    rep(i,0,n) {
-        cin>>a[i];
-        if (check[a[i]]>0) ch=true;
-        check[a[i]]++;
-    }
-    if (!ch) {
-        sort(a.begin(),a.end(),greater<int>());
-        print("YES");
-        rep(i,0,n) prints(a[i]);
-        cout<<"\n";
-        return;
-    }
-    if (ch and n==2) {
 
-        print("NO");
-        return;
-    }
-    sort(a.begin(),a.end(),greater<int>());
-    for (int i=1;i<n-i;i++) {
-        swap(a[i],a[n-i]);
-    }
-    int su=0;bool c=false;
-    for (int i=0;i<n;i++) {
-        if (a[i]==su) {
-            c=true;
-            break;
-        }
-        su+=a[i];
-    }
-    if (c) {
-        print("NO");
-    }
-    else {
-        print("YES");
-        rep(i,0,n) prints(a[i]);
-        cout<<"\n";
-    }
+
+
+void solve() {
+    ll x,y;
+    cin>>x>>y;
+    if (y<x) print(x+y);
+    else if (y==x) print(x);
+    else print(y-(y%x)/2);
     return;
 }
 
--- test2.cpp
+++ test2.cpp
@@ -5,72 +5,20 @@ typedef unsigned long long ull;
 #define print(x) cout<<x<<'\n'
 #define printe(x) cout<<x<<endl
 #define prints(x) cout<<x<<" "
-#define rep(i,a,b) for (int i=a;i<b;i++)
+#define repi(i,a,b) for (int i=a;i<b;i++)
+#define repl(i,a,b) for (ll i=a;i<b;i++)
 #define maxVec(v) *max_element(v.begin(),v.end())
 #define minVec(v) *min_element(v.begin(),v.end())
 #define bpcnt(a) __builtin_popcount(a)
 #define bpcntll(a) __builtin_popcountll(a)
 inline ll lsb(ll n) {return n&-n;}
 inline ll msb(ll n) {return (1 << (31 - __builtin_clz(n)));}
-// bool sortbysec(const pair<ll,ll> &a,
-// 			const pair<ll,ll> &b)
-// {
-// 	return (a.second < b.second);
-// }
-// Fill whole array with 0.
-// memset(arr, 0, n*sizeof(arr[0]));
-
-void solve() {
-    int n;
-	cin>>n;
-	int i=1,j=n*n;
-	bool ct=true;
-	if (n%2) {
-	for (int r=0;r<n;r++) {
-		for (int c=0;c<n;c++) {
-			if (ct) {
-				prints(i);
-				i++;
-				ct=false;
-			}
-			else {
-				prints(j);
-				j--;
-				ct=true;
-			}
-		}
-		cout<<"\n";
-	}
-	}
-	else {
-		for (int r=0;r<n;r++) {
-		for (int c=0;c<n;c++) {
-			if (ct) {
-				prints(i);
-				i++;
-				ct=false;
-			}
-			else {
-				prints(j);
-				j--;
-				ct=true;
-			}
-		}
-		ct=!(ct);
-		cout<<"\n";
-	}
-	}
-    return;
-}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int it,t=1;
-    cin>>t;
-    for (it=0;it<t;it++) {
-        solve();
-    }
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	return 0;
 }
