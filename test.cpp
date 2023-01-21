589abd483c7cc48cff8d26964668acb7708f43b8
aaryan200 <co21btech11001@iith.ac.in>
2023-01-21 23:05:49 +0530

    commit

--- test.cpp
+++ test.cpp
@@ -20,15 +20,32 @@ inline ll msb(ll n) {return (1 << (31 - __builtin_clz(n)));}
 // Fill whole array with 0.
 // memset(arr, 0, n*sizeof(arr[0]));
 
+const int N = 1.0e4+5;
+int dp[N];
 
-
+bool solution(vector<int>& a, vector<int>& b, int x, int& n) {
+    if (dp[x]!=-1) return dp[x];
+    if (x==0) return dp[x]=1;
+    rep(i,0,n) {
+        if (b[i]>0) {
+            int xn = x-a[i];
+            b[i]--;
+            if (xn>=0 and solution(a,b,xn,n)==1) return dp[x]=1;
+            b[i]++;
+        }
+    }
+    return dp[x]=0;
+}
 
 void solve() {
-    ll x,y;
-    cin>>x>>y;
-    if (y<x) print(x+y);
-    else if (y==x) print(x);
-    else print(y-(y%x)/2);
+    memset(dp,-1,N*sizeof(int));
+    int n,x;
+    cin>>n>>x;
+    vector<int> a(n,0);
+    vector<int> b(n,0);
+    rep(i,0,n) cin>>a[i]>>b[i];
+    if (solution(a,b,x,n)) print("Yes");
+    else print("No");
     return;
 }
 
@@ -37,7 +54,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int it,t=1;
-    cin>>t;
+    // cin>>t;
     for (it=0;it<t;it++) {
         solve();
     }
--- test1.cpp
+++ test1.cpp
@@ -1,78 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define print(x) cout<<x<<endl
 typedef long long ll;
 typedef unsigned long long ull;
+#define print(x) cout<<x<<'\n'
+#define printe(x) cout<<x<<endl
+#define prints(x) cout<<x<<" "
+#define rep(i,a,b) for (int i=a;i<b;i++)
+#define maxVec(v) *max_element(v.begin(),v.end())
+#define minVec(v) *min_element(v.begin(),v.end())
+#define bpcnt(a) __builtin_popcount(a)
+#define bpcntll(a) __builtin_popcountll(a)
+inline ll lsb(ll n) {return n&-n;}
+inline ll msb(ll n) {return (1 << (31 - __builtin_clz(n)));}
 // bool sortbysec(const pair<ll,ll> &a,
 // 			const pair<ll,ll> &b)
 // {
 // 	return (a.second < b.second);
 // }
+// Fill whole array with 0.
+// memset(arr, 0, n*sizeof(arr[0]));
 
-ll pow2(ll n) {
-    ll out=0;
-    while (n%2==0) {
-        out++;
-        n=n/2;
+ll moduloMultiplication(ll a, ll b, ll mod) {
+    ll res = 0;
+    a %= mod;
+    while (b) {
+        if (b & 1) res = (res + a) % mod;
+        a = (2 * a) % mod;
+        b >>= 1;
     }
-    return out;
+    return res;
 }
 
-ll pow5(ll n) {
-    ll out=0;
-    while (n%5==0) {
-        out++;
-        n=n/5;
-    }
-    return out;
-}
+const ll md = 1.0e9+7;
 
-void solve(int it) {
-    ll n,m,p2,p5,req=1;
-    cin>>n>>m;
-    if (it==902) {
-        cout<<n<<m<<endl;
+void solve() {
+    ll n;
+    cin>>n;
+    ll ans = n*(n-1);
+    for (ll i=1;i<=n;i++) {
+        ans = moduloMultiplication(ans,i,md);
     }
-    if (m==1) {
-        print(n);
-        return;
-    }
-    p2=pow2(n); p5=pow5(n);
-    ll initz=min(p2,p5);
-    for (ll i=0;i<p5;i++) req=req*2;
-    for (ll i=0;i<p2;i++) req=req*5;
-    if (req>m) {
-        ll temp=req,c1=p5,c2=p2;
-        while (c2>0 and temp>m) {
-            c2--;
-            temp=temp/5;
-        }
-        while (c1>0 and temp>m) {
-            c1--;
-            temp=temp/2;
-        }
-        ll maxp=c1+c2,temp2=1;
-        for (ll i=0;i<maxp;i++) temp2=temp2*2;
-        for (ll po5=1;po5<=maxp;po5++) {
-            temp2=temp2*5/2;
-            if (temp2>m) break;
-        }
-        ll req1;
-        if (temp2>m) req1=temp2*2/5;
-        else req1=temp2;
-        print(n*(m-m%req1));
-        return;
-    }
-    if (req<=m) {
-        // print(req);
-        ll temp=req;
-        while (temp<m) temp=temp*10;
-        if (temp>m) temp=temp/10;
-        // print(temp);
-        ll out=m-m%temp;
-        print(n*out);
-        return;
-    }
+    print(ans);
     return;
 }
 
@@ -83,7 +51,7 @@ int main()
     int it,t=1;
     cin>>t;
     for (it=0;it<t;it++) {
-        solve(it);
+        solve();
     }
     return 0;
 }
